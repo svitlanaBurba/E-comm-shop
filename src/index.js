ba8e84afaa921a6ec0955709d59d8a6582949671
Svitlana Burba <svitlana.burba@gmail.com>
2021-09-16 23:30:44 +0300

    WP added

---
+++ src/index.js
@@ -0,0 +1,7 @@
+  
+import './styles/styles.scss';
+// import './styles/css.css'
+import * as $ from 'jquery'
+import json from './assets/json.json'
+import {colorswitch} from './js/colorswitch'; 
+console.log(json)
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+const optimization = ()=>{
+  const config = {
+    splitChunks: {
+      chunks: 'all',
+},
+  }
+  if (isProd) {config.minimizer = [
+    new OptimizeCssAssetsWebpackPlugin(),
+    new TerserWebpackPlugin()
+  ]}
+  return config
+}
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+      extensions: ['.js', '.json', '.png']
+    },
+    optimization: optimization(),
+    plugins: [
+      new HTMLWebpackPlugin({
+        // title: 'E-commerce Shop',
+        template: './index.html',
+        minify: {
+          collapseWhitespace:isProd
+        }
+      }),
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+        chunkFilename: '[name].[id].[contenthash].css',
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use:[ MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+        },
+        {
+          test: /\.scss$/,
+          use: [  MiniCssExtractPlugin.loader,
+            'css-loader',
+            'postcss-loader',
+            'sass-loader'],
+        },
+        {
+          test: /\.(png|jpe?g|svg|gif)$/i,
+          use: ['file-loader'],
+        },
+        {
+          test: /\.(ttf|woff|woff2|eot)$/i,
+          use: ['file-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {loader:'babel-loader', options: {
+            presets: ['@babel/preset-env']
+          }},
+        }
+      ]
+    },
+    devServer: {
+      port: 5500,
+      hot: isDev
+    },
+}
