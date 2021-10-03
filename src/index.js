6e4b35aaf5972172dedeceb5f10ba3e9d78eb961
Svitlana Burba <svitlana.burba@gmail.com>
2021-10-04 00:08:23 +0300

    Added cart sidebar, rendering of products; refactored filter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './styles/styles.scss';
-import CountdownTimer from './scripts/timer';
+
 import onCheckout1Load from './scripts/checkout1';
 import onCheckout2Load from './scripts/checkout2';
 import onMainLoad from './scripts/main';
+
+
 
 const page = document.getElementsByTagName("head")[0].dataset['page'];
 
@@ -15,11 +17,7 @@ if (page === 'checkout1') {
 }
 
 if (page === 'index') {
-  onMainLoad();
 
-  const timer = new CountdownTimer({
-    selector: '#timer-main',
-    targetDate: new Date('Oct 05, 2021')
-  });
-  timer.start();
+  window.addEventListener('DOMContentLoaded', onMainLoad);
+
 }
---
+++ src/scripts/products/products.js
@@ -0,0 +1,52 @@
+const products = [
+  {
+    id: 1,
+    name: 'White Long Dress With Train',
+    price: 2285,
+    image: './assets/products/white-dress.jpg',
+    categories: ['Wedding Dresses', 'Jewelry'],
+    popular: true
+  },
+  {
+    id: 2,
+    name: 'Black Classic Suit',
+    price: 1855,
+    image: './assets/products/black-wedding-suit.jpg',
+    categories: ['Wedding Suits'],
+    popular: true
+  },
+  {
+    id: 3,
+    name: 'Beautiful Glasses For Newlyweds',
+    price: 168,
+    image: './assets/products/wedding-glasses.jpg',
+    categories: ['Accessories'],
+    popular: true
+  },
+  {
+    id: 4,
+    name: 'Candles For The Ceremony',
+    price: 69,
+    image: './assets/products/wedding-candles.jpg',
+    categories: ['Decorations'],
+    popular: true
+  },
+  {
+    id: 5,
+    name: 'White Long Dress With Train',
+    price: 2795,
+    image: './assets/products/white-dress-train.jpg',
+    categories: ['Wedding Dresses'],
+    popular: true
+  },
+  {
+    id: 6,
+    name: 'Navy Classic Suit',
+    price: 1318,
+    image: './assets/products/navy-suit.jfif',
+    categories: ['Wedding Suits', 'Wedding Shoes'],
+    popular: true
+  }
+];
+
+export default products;
